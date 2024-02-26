ccd9df6da7d2ce2617dca48610b3e079c9d21f75
frnzxz7 <144467096+palaginivan@users.noreply.github.com>
2024-02-26 22:27:34 +0300

    Add files via upload

---
+++ dzshka/apxuBaTopchik.py
@@ -0,0 +1,28 @@
+def apxuBaTop():
+    with open("C:\\dzshka\\do.txt", "r", encoding="utf-8") as f:
+        txt = f.read()
+
+    z_prepinaniya = {'.', ',', ';', ':', '!', '?'}
+    posle = ''
+    probel = False
+
+    for znak in txt:
+        if probel:
+            if znak != ' ':
+                posle += znak
+                probel = False
+        else:
+            if znak in z_prepinaniya:
+                probel = True
+                posle += znak
+            elif znak != ' ':
+                posle += znak
+            else:
+                # Добавляем пробел только если предыдущий символ не является знаком препинания
+                if posle and posle[-1] not in z_prepinaniya:
+                    posle += znak
+
+    with open("C:\\dzshka\\posle.txt", "w") as f:
+        f.write(posle)
+
+apxuBaTop()
